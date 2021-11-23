c5acde4d188d10fe32ff8990e219cc90ffd7c02f
SelinCNZ <selin.cengiz.tr@gmail.com>
2021-11-23 15:26:50 +0100

    NEW PROJECT

---
+++ monRDV-boot/src/main/java/sopra/formation/MonRdvBootApplication.java
@@ -0,0 +1,13 @@
+package sopra.formation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonRdvBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonRdvBootApplication.class, args);
+	}
+
+}
