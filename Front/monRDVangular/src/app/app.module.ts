5531dea75951e85adbe8215dcc0bcc1439cc631b
SelinCNZ <selin.cengiz.tr@gmail.com>
2021-12-01 11:40:05 +0100

    Création composant et service

--- Front/monRDVangular/src/app/app.module.ts
+++ Front/monRDVangular/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PatientComponent } from './patient/patient.component';
+import { PraticienComponent } from './praticien/praticien.component';
+import { ConsultationComponent } from './consultation/consultation.component';
+import { MotifComponent } from './motif/motif.component';
+import { CreneauComponent } from './creneau/creneau.component';
+import { LieuComponent } from './lieu/lieu.component';
+import { SpecialiteComponent } from './specialite/specialite.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientComponent,
+    PraticienComponent,
+    ConsultationComponent,
+    MotifComponent,
+    CreneauComponent,
+    LieuComponent,
+    SpecialiteComponent
   ],
   imports: [
     BrowserModule,
---
+++ Front/monRDVangular/src/app/lieu/lieu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lieu',
+  templateUrl: './lieu.component.html',
+  styleUrls: ['./lieu.component.scss']
+})
+export class LieuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
