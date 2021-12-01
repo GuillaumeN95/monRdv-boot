7f3906f414c9d34695e8e6603d85f774c184b18f
SelinCNZ <selin.cengiz.tr@gmail.com>
2021-12-01 12:19:08 +0100

    Patient

--- Front/monRDVangular/src/app/consultation/consultation.service.ts
+++ Front/monRDVangular/src/app/consultation/consultation.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AppConfigService } from '../app-config.service';
+import { Consultation } from '../model';
 
 @Injectable({
   providedIn: 'root'
--- Front/monRDVangular/src/app/model.ts
+++ Front/monRDVangular/src/app/model.ts
@@ -18,3 +18,46 @@ export class Consultation
 
     }
 }
+
+export class Patient
+{
+    id: number;
+    version: number;
+    //civilite: string;
+    nom: string;
+    prenom: string;
+    dtNaissance: string;
+    email: string;
+    telephone: string;
+    principal:boolean;
+    adresse: Adresse;
+
+
+
+    constructor(id?: number,version?: number,nom?: string,prenom?: string,dtNaissance?: string,email?: string,telephone?: string,principal?:boolean,adresse?: Adresse){
+        this.id = id;
+        this.version = version;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dtNaissance=dtNaissance;
+        this.email = email;
+        this.telephone = telephone;
+        this.principal=principal;
+        this.adresse = adresse;
+
+    }
+}
+
+export class Adresse {
+    voie: string;
+    complement: string;
+    codePostal: string;
+    ville: string;
+
+    constructor(voie?: string, complement?: string, codePostal?: string, ville?: string) {
+        this.voie = voie;
+        this.complement = complement;
+        this.codePostal = codePostal;
+        this.ville = ville;
+    }
+}
