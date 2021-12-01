bfd789e6b0e5554b7b004aed4db0c72664ffc7b3
Guillaume <nguyen.guillaume@outlook.com>
2021-12-01 12:08:21 +0100

    Creneau

--- Front/monRDVangular/src/app/creneau/creneau.component.ts
+++ Front/monRDVangular/src/app/creneau/creneau.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Creneau } from '../model';
+import { CreneauService } from './creneau.service';
+
 
 @Component({
   selector: 'app-creneau',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreneauComponent implements OnInit {
 
-  constructor() { }
+  creneauForm: Creneau;
+
+  constructor(private creneauService: CreneauService) { }
 
   ngOnInit(): void {
+  }
+
+  list(): Array<Creneau>{
+    return this.creneauService.findAll();
+  }
+  
+  add(){
+    this.creneauForm = new Creneau();
+    
+  }
+  
+  edit(id: number){
+    this.creneauService.findById(id).subscribe(response => {
+      this.creneauForm = response;
+    }, err => console.log(err));
+  }
+  
+  save(){
+    if(this.creneauForm.id){
+      this.creneauService.modify(this.creneauForm);
+    } else {
+      this.creneauService.create(this.creneauForm);
+    }
+  
+    this.cancel();
+  }
+  
+  cancel(){
+    this.creneauForm = null;
+  }
+  
+  remove(id: number){
+    this.creneauService.deleteById(id);
   }
 
 }
--- Front/monRDVangular/src/app/creneau/creneau.service.ts
+++ Front/monRDVangular/src/app/creneau/creneau.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import { Creneau } from '../model';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AppConfigService } from './../app-config.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CreneauService {
 
-  constructor() { }
+  creneaux : Array<Creneau> = new Array<Creneau>();
+  creneauUrl: string;
+
+  constructor(private http: HttpClient, private appConfig: AppConfigService) {
+    this.creneauUrl = this.appConfig.backEndUrl + "creneau/"
+    this.load();
+   }
+   
+
+   findAll(): Array<Creneau> {
+    return this.creneaux;
+  }
+
+  findById(id: number): Observable<Creneau> {
+    return this.http.get<Creneau>(this.creneauUrl + id);
+  }
+
+  create(creneau: Creneau) {
+    this.http.post<Creneau>(this.creneauUrl, creneau).subscribe(resp => {
+      this.load();
+    }, error => console.log(error));
+  }
+
+  modify(creneau: Creneau) {
+    this.http.put<Creneau>(this.creneauUrl + creneau.id, creneau).subscribe(resp => {
+      this.load();
+    }, error => console.log(error));
+  }
+
+  deleteById(id: number) {
+    this.http.delete<void>(this.creneauUrl + id).subscribe(resp => {
+      this.load();
+    }, error => console.log(error));
+  }
+
+  load() {
+    this.http.get<Array<Creneau>>(this.creneauUrl).subscribe(response => {
+      this.creneaux = response;
+    }, error => console.log(error));
+  }
+
 }
