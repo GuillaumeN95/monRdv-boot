6dd599d10c8124a88422dd9a4cedae637d3351df
SelinCNZ <selin.cengiz.tr@gmail.com>
2021-12-01 11:17:47 +0100

    Coorection views

--- monRDV-boot/src/main/java/sopra/formation/model/Views.java
+++ monRDV-boot/src/main/java/sopra/formation/model/Views.java
@@ -6,11 +6,17 @@ public class Views {
 
 	public static class ViewAdresse extends ViewCommon {
 	}
+	
+	public static class ViewAdresseDetail extends ViewAdresse {
+	}
 
 	public static class ViewCivilite extends ViewCommon {
 	}
 	
 	public static class ViewConsultation extends ViewCommon {
+	}
+	
+	public static class ViewConsultationDetail extends ViewConsultation {
 	}
 	
 	public static class ViewCreneau extends ViewCommon {
@@ -22,13 +28,25 @@ public class Views {
 	public static class ViewLieu extends ViewCommon {
 	}
 	
+	public static class ViewLieuDetail extends ViewLieu {
+	}
+	
 	public static class ViewMotif extends ViewCommon {
+	}
+	
+	public static class ViewMotifDetail extends ViewMotif {
 	}
 	
 	public static class ViewPatient extends ViewCommon {
 	}
 	
+	public static class ViewPatientDetail extends ViewPatient {
+	}
+	
 	public static class ViewPraticien extends ViewCommon {
+	}
+	
+	public static class ViewPraticienDetail extends ViewPraticien {
 	}
 	
 	public static class ViewSecteur extends ViewCommon {
@@ -37,9 +55,15 @@ public class Views {
 	public static class ViewSpecialite extends ViewCommon {
 	}
 	
+	public static class ViewSpecialiteDetail extends ViewSpecialite {
+	}
+	
 	public static class ViewType extends ViewCommon {
 	}
 	
 	public static class ViewUtilisateur extends ViewCommon {
+	}
+	
+	public static class ViewUtilisateurDetail extends ViewUtilisateur {
 	}
 }
